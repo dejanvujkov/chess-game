353a6c42c221d8bf6ab33f64498b2ba98ec428d6
Dejan Vujkov <vujkovdejan@gmail.com>
2021-12-13 18:48:47 +0100

    Added common components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HistoryComponent } from './history/history.component';
+import { TableComponent } from './table/table.component';
+import { PileComponent } from './pile/pile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent,
+    TableComponent,
+    PileComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pile/pile.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PileComponent } from './pile.component';
+
+describe('PileComponent', () => {
+  let component: PileComponent;
+  let fixture: ComponentFixture<PileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PileComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
